a2ff88a75c0f110af3688322412a8ad647209353
augustobarbosa77 <augusto.carlos@omnik.com.br>
2023-04-17 21:51:14 -0300

    [002] tela de login / estilização

---
+++ src/views/Planes.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      <span> TODO: Continuar com a criação da página de cadastro - Selecionar Plano </span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+    },
+  };
+  </script>
+  
